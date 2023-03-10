3b8109c7ad69ec87baf161ae5bc5e75e7969c0ec
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2023-03-10 17:49:05 +0700

    Update

--- app/controllers/addStockController/addStock.js
+++ app/controllers/addStockController/addStock.js
@@ -19,7 +19,7 @@ const controller = async (req, res) => {
 		await db.transaction(async trx => {
 			const checkAddStock = await getAddStock(body.c_login, trx)
 
-			if(!checkAddStock && (body.q_employee_card == "0" && body.q_master_card == "0" && body.q_tenant_card == "0")){
+			if(!checkAddStock && body.q_employee_card == "0" && body.q_master_card == "0" && body.q_tenant_card == "0"){
 				return res.status(200).send({
 					status: '00',
 					message: 'success',
